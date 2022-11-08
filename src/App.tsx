f7f92bd024a9da4662bb2aac20c1cc5cd85e3ec0
Hannah.Celemen <hcelemen@icloud.com>
2022-11-08 15:14:15 -0700

    Install TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+export default function App() {
+  return (
+    <main>
+      <h1>Manga Register</h1>
+      <div>Container</div>
+    </main>
+  );
+}
